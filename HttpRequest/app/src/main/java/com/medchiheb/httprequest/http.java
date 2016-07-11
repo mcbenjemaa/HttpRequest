5449bd55fe88305de7f95714c3c412095a4c7785
mc.benjemaa@gmail.com <mc.benjemaa@gmail.com>
2016-07-11 17:10:50 +0100

    HttpRequest

---
+++ HttpRequest/app/src/main/java/com/medchiheb/httprequest/http.java
@@ -0,0 +1,10 @@
+package com.medchiheb.httprequest;
+
+/**
+ * Created by MED CHIHEB on 07/04/2016.
+ */
+public class http {
+
+    String apikey="52272760212e69f5b4c15f83f139b2c9c2d32f93";
+
+}
---
+++ HttpRequest/httprequest.1.0/src/main/java/com/medchiheb/httprequest/HttpRequest.java
@@ -0,0 +1,146 @@
+package com.medchiheb.httprequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by MED CHIHEB on 06/04/2016.
+ */
+public class HttpRequest
+{
+    public static final String POST_METHOD="POST";
+    public static final String GET_METHOD="GET";
+private JSONObject obj;
+
+   public HttpRequest(){
+       obj=new JSONObject();
+   }
+public JSONObject MakeHttpRequest(String Method,String url, HashMap<String,String> parms)throws JSONException,IOException{
+    if(Method.equals(POST_METHOD)){
+        return new JSONObject(downloadUrlPOST(Method,url,parms));
+    }else{
+        return new JSONObject(performGETCall(url));
+    }
+
+}
+    private String downloadUrlPOST(String method,String myurl, HashMap<String,String> parms) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod(method);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(parms));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            conn.connect();
+
+            int response = conn.getResponseCode();
+           // Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    private String  performGETCall(String requestURL) {
+
+        URL url;
+        String response = "";
+        try {
+            url = new URL(requestURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+
+            conn.connect();
+
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else {
+                response="";
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    private String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+}
